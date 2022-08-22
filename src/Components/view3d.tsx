a28ee5b7dc75f8c43aa166fa0ba6016a11d67759
Nikita <sliverregulyar@gmail.com>
2022-08-23 01:35:09 +0300

    add light color

--- src/Components/view3d.tsx
+++ src/Components/view3d.tsx
@@ -4,18 +4,25 @@ import { useRef } from 'react';
 import * as THREE from 'three';
 import { OrbitControls } from '../ThreeJs/OrbitControls';
 import { GLTFLoader } from '../ThreeJs/GLTFLoader';
+import { Camera } from 'three';
+
+//const _MODEL= 'models/my2/puff.gltf'
+const _MODEL= 'models/sofa4/scene.gltf'
 
 export default function My3DView() {
     const [loading,setLoading]=useState(true);
     const render3d=useRef(true);
     const canvasDiv: { current: HTMLDivElement | null } = useRef(null);
-    let scene: THREE.Scene, camera: THREE.Camera, renderer: THREE.WebGL1Renderer, hlite, loader: GLTFLoader;   
+    let  hlite, loader: GLTFLoader;   
 
+    
+    //BASE IMG ARRAY
     const [file,setFile]=useState([
         'https://images.wallpaperscraft.com/image/single/room_vanity_flooring_wall_80351_3840x2400.jpg',       
         'https://i.pinimg.com/originals/34/99/d1/3499d12f28a741f0063ee8f2bbd711d9.jpg'][Math.round(Math.random()*1)]
     );
 
+    //LOAD FILE IMG
     const imgRef:{current:HTMLImageElement|undefined}=useRef();
     useEffect(()=>{
         const img=new Image();
@@ -26,6 +33,22 @@ export default function My3DView() {
     },[file])
     
     const rendererRef:{current:THREE.WebGL1Renderer|undefined}=useRef();
+    const directionalLightRef:{current:THREE.DirectionalLight|undefined}=useRef();
+    const cameraRef:{current:THREE.Camera|undefined}=useRef();
+    const sceneRef:{current:THREE.Scene|undefined}=useRef();
+
+    const [light,setLight]=useState('');
+
+    useEffect(()=>{
+        if(sceneRef.current && light){
+            if(directionalLightRef.current) sceneRef.current?.remove(directionalLightRef.current)
+            directionalLightRef.current = new THREE.DirectionalLight(light, 3);
+            directionalLightRef.current.position.set(12, 12, 12);
+            directionalLightRef.current.castShadow = true;
+            sceneRef.current!.add(directionalLightRef.current);
+        }
+
+    },[light])
    
     useEffect(() => {
 
@@ -37,25 +60,32 @@ export default function My3DView() {
             }           
 
             //SCENE
-            scene = new THREE.Scene();         
-            //scene.background = new THREE.Color(0xdddddd);
-            //scene.add(new THREE.GridHelper(10, 10)) // ДОБАВЛЕНИЕ СЕТКИ          
+            sceneRef.current = new THREE.Scene();         
+            //sceneRef.current.background = new THREE.Color(0xdddddd);
+            //sceneRef.current.add(new THREE.GridHelper(10, 10)) // ДОБАВЛЕНИЕ СЕТКИ          
     
             //CAMERA
             const {width,height}= canvasDiv.current!.getBoundingClientRect();
-            //camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 10000);
-            camera = new THREE.PerspectiveCamera(40, width / height, 1, 10000);
+            //cameraRef.current = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 10000);
+            cameraRef.current = new THREE.PerspectiveCamera(40, width / height, 1, 10000);
             
-            camera.position.set(0, 0, 6);
-            // camera.lookAt(0, 22, 1);     
+            cameraRef.current.position.set(0, 0, 6);
+            // cameraRef.current.lookAt(0, 22, 1);     
     
             //LIGHT
-            let directionalLight = new THREE.DirectionalLight('white', 3);
-            directionalLight.position.set(12, 12, 12);
-            directionalLight.castShadow = true;
-            scene.add(directionalLight);
+            // let directionalLight = new THREE.DirectionalLight('white', 3);
+            // directionalLight.position.set(12, 12, 12);
+            // directionalLight.castShadow = true;
+            // sceneRef.current.add(directionalLight);
+
+            directionalLightRef.current = new THREE.DirectionalLight('white', 3);
+            directionalLightRef.current.position.set(12, 12, 12);
+            directionalLightRef.current.castShadow = true;
+            sceneRef.current.add(directionalLightRef.current);
+            //sceneRef.current.remove(directionalLight);
     
-            hlite = new THREE.AmbientLight(0x404040, 20);
+            hlite = new THREE.AmbientLight(0x404040, 1);
+            sceneRef.current.add(hlite);
             
             rendererRef.current = new THREE.WebGL1Renderer({ antialias: true ,alpha:true});
             rendererRef.current.setSize(window.innerWidth, window.innerHeight);
@@ -63,7 +93,7 @@ export default function My3DView() {
            
     
             //CONTROL
-            let control=new OrbitControls(camera,rendererRef.current.domElement);
+            let control=new OrbitControls(cameraRef.current,rendererRef.current.domElement);
             control.maxDistance=110;
             control.minDistance=2.2;
             control.getPolarAngle()
@@ -79,9 +109,9 @@ export default function My3DView() {
             //LOADER gLTF
             loader = new GLTFLoader();
             setLoading(true);
-            loader.load(process.env.PUBLIC_URL + 'models/sofa4/scene.gltf', (gltf: any) => {
+            loader.load(process.env.PUBLIC_URL + _MODEL, (gltf: any) => {
                 setLoading(false);            
-                scene.add(gltf.scene);            
+                sceneRef.current!.add(gltf.scene);            
                 const model = gltf.scene.children[0];
                 model.position.set(0,-.5,0);
                 model.scale.set(1, 1, 1);
@@ -90,8 +120,8 @@ export default function My3DView() {
             })
             //RENDER
             function render() {
-                if(render3d.current){
-                rendererRef.current!.render(scene, camera);
+                if(render3d.current&& sceneRef.current){
+                rendererRef.current!.render(sceneRef.current, cameraRef.current as Camera);
                 requestAnimationFrame(render); 
                 control.update();
                 console.log('render 3D');   
@@ -121,10 +151,14 @@ export default function My3DView() {
         <div className="Item">                         
             {loading&&<div className='Item__loading'>Loading</div>}            
             <div className='Item__div-canvas' ref={canvasDiv} >
-            <input className='Item__file'  type={"file"} onChange={(event:any)=>{                
+                <div className='Item__menu' >
+                <input  type={"file"} onChange={(event:any)=>{                
                 console.log(event.target.files[0]);
                 setFile(URL.createObjectURL(event.target.files[0]))
                 }} />
+                <input type={'color'} onChange={event=>setLight(event.target.value)}  value={light}/>
+                </div>
+            
             <img src='' alt="" ref={imgRef as {current:HTMLImageElement}} />    
             </div>           
         </div>
